54d908d8b3849d7632cdbf9f630ff4cc568cd222
bressar <dougbressar@gmail.com>
2024-10-23 10:07:03 +0100

    com2

---
+++ src/domain/base.py
@@ -0,0 +1,11 @@
+from uuid import UUID, uuid4 # UIDs (Universally Unique Identifiers) o uuid4 gera um id aleatório -> ex.: b59b58f3-522a-40ff-857a-c6a42dbfd979
+""" UUIDs são comumente utilizados em contextos como:
+    - Identificadores únicos em bancos de dados distribuídos.
+    - Identificadores únicos em APIs.
+    - Garantia de que os IDs gerados por diferentes sistemas não entrem em conflito """
+    
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class DomainBase(BaseModel): # cria um Id e uma date time toda vez que a classe for instanciada
+    pass
